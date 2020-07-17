4e9846fa959926e656131091d10f542e6cf54a75
Faizan Ahmad <ahmadfaizan1604@gmail.com>
2020-07-17 08:14:51 +0530

    29 - Serialize Django Model

--- tweets/views.py
+++ tweets/views.py
@@ -23,7 +23,7 @@ def tweet_create_view(request, *args, **kwargs):
         # do other form related logic
         obj.save()
         if request.is_ajax():
-            return JsonResponse({}, status=201)
+            return JsonResponse(obj.serialize(), status=201) # 201 == created items
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
@@ -36,7 +36,7 @@ def tweet_list_view(request, *args, **kwargs):
     return JSON data
     """
     qs = Tweet.objects.all()
-    tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
+    tweet_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweet_list
