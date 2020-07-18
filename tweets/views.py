318c8d1a37d4b32b648b8136f9af0d180b3c7cfa
Faizan Ahmad <ahmadfaizan1604@gmail.com>
2020-07-18 13:16:17 +0530

    36 - Permissions & Roadmap

--- tweets/views.py
+++ tweets/views.py
@@ -14,6 +14,9 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    '''
+    REST API Create View -> DRF
+    '''
     user = request.user
     if not request.user.is_authenticated:
         user = None
