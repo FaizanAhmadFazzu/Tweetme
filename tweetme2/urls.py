384f3acf69983603bc3586aaeccd796ef8e9c8bd
Faizan Ahmad <ahmadfaizan1604@gmail.com>
2020-07-24 10:42:28 +0530

    45 - JavaScript Tweet Action Handler

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path # re_path
 from tweets.views import (
     home_view,
+    tweet_action_view,
     tweet_delete_view,
     tweet_create_view,
     tweet_detail_view,
@@ -13,5 +14,6 @@ urlpatterns = [
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
     path('api/tweets/<int:tweet_id>', tweet_delete_view),
 ]
