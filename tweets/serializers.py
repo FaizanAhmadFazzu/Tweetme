02a9ef4889931b8fa57df1ee2275d467326b5650
Faizan Ahmad <ahmadfaizan1604@gmail.com>
2020-07-24 14:57:35 +0530

    47 - Retweeting Logic

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -9,11 +9,13 @@ TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
+    content = serializers.CharField(allow_blank=True, required=False)
 
-    def validate_content(self, value):
-        value = value.lower().stripe() # "like " -> "like"
+    def validate_action(self, value):
+        value = value.lower().strip() # "like " -> "like"
         if not value in TWEET_ACTION_OPTIONS:
             raise serializers.ValidationError("This is not a valid action for tweets")
+        return value
         
 
 class TweetSerializer(serializers.ModelSerializer):
