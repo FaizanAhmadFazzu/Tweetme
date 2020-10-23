721b3a752f8d8e7cac34e0c8948dfc36e7e65e1c
Faizan Ahmad <ahmadfaizan1604@gmail.com>
2020-10-23 09:06:32 +0530

    48 -Two Types of Serializers

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from django.urls import path # re_path
-from tweets.views import home_view, tweet_create_view, tweet_list_view, tweet_detail_view 
+from django.urls import path, re_path # url()
+from tweets.views import (
+    home_view, 
+    tweet_action_view,
+    tweet_delete_view,
+    tweet_detail_view, 
+    tweet_list_view,
+    tweet_create_view,
+) 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
+    path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
 ]
