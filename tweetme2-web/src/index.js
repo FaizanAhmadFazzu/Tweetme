7b2c485aaa16bc370a5c55be1728ed9bcb41af92
Faizan  Ahmad Khan <faizan.ahmad@instabeans.com>
2021-01-01 14:46:24 +0530

    63 - Adjust the React Render Process

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
+import {TweetsComponent} from './tweets'
 import reportWebVitals from './reportWebVitals';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const appE1 = document.getElementById('root');
+if (appE1) {
+    ReactDOM.render(<App />, appE1);
+}
+const tweetsE1 = document.getElementById('tweetme-2')
+if (tweetsE1){
+    ReactDOM.render(<TweetsComponent />, tweetsE1);
+}
 
 
 // If you want to start measuring performance in your app, pass a function
