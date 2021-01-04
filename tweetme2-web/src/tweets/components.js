dc853b787d86ae5a42b520272dc6e9175dff4007
Faizan  Ahmad Khan <faizan.ahmad@instabeans.com>
2021-01-04 14:57:40 +0530

    73 - Improving the Tweet Action Button

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -73,8 +73,9 @@ export function TweetsList(props) {
 }
 
 export function ActionBtn(props){
-    const {tweet, action} =  props 
-    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
+    const {tweet, action, didPerformAction} =  props 
+    const likes = tweet.likes ? tweet.likes : 0
+
     // const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
     const actionDisplay = action.display ? action.display : 'Action'
@@ -82,9 +83,8 @@ export function ActionBtn(props){
 
     const handleActionBackendEvent = (response, status) => {
         console.log(response, status)
-        if (status === 200){
-            setLikes(response.likes)
-            // setUserLike(true)
+        if ((status === 200 || status === 201) && didPerformAction){
+            didPerformAction(response, status)
         }
     }
     const handleClick = (event) => {
@@ -96,24 +96,36 @@ export function ActionBtn(props){
  
 export function ParentTweet(props){
     const {tweet} = props
-    return tweet.parent ? <div className='col-11 mx-auto p-3 border rounded'>
+    return tweet.parent ? <div className='row'>
+        <div className='col-11 mx-auto p-3 border rounded'>
         <p className='mb-0 text-muted small'>Retweet</p>
         <Tweet className={' '} tweet={tweet.parent} />
+    </div>
     </div> : null 
 }
   
 export function Tweet(props) {
     const {tweet} = props
+    const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null)
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+
+    const handlePerformAction = (newActiontweet, status) => {
+        if (status === 200) {
+            setActionTweet(newActiontweet)
+        } else if (status === 201) {
+            // let the tweet list know
+        }
+    }
     return <div className={className}>
             <div>
                 <p>{tweet.id} - {tweet.content}</p>
                 <ParentTweet tweet={tweet} />
             </div>
-            <div className='btn btn-group'>
-                <ActionBtn tweet={tweet} action={{type: "like", display: "Likes"}} />
-                <ActionBtn tweet={tweet} action={{type: "unlike", display: "Unlike"}} />
-                <ActionBtn tweet={tweet} action={{type: "retweet", display:"Retweet"}} />
+            {actionTweet && <div className='btn btn-group'>
+                <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "like", display: "Likes"}} />
+                <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "unlike", display: "Unlike"}} />
+                <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "retweet", display:"Retweet"}} />
             </div>
+        }
     </div>
     }
